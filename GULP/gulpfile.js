db0d5e6d8e5bb069b5af7910f8a12f45d5f8815b
Ali Assim Mokdad <49646139+VALAK08@users.noreply.github.com>
2023-10-19 23:06:56 +0300

    Create gulpfile.js

--- GULP/gulpfile.js
+++ GULP/gulpfile.js
@@ -1,8 +1,12 @@
 ﻿const gulp = require('gulp');
 const minifyHtml = require('gulp-minify-html');
+const rename = require('gulp-rename');
 
 gulp.task('minify-html', function () {
-    return gulp.src('Shared/_Layout.html')
-        .pipe(minifyHtml())  
-        .pipe(gulp.dest('dist'));
+    return gulp.src('Pages/Shared/_Layout.cshtml')  
+        .pipe(minifyHtml())
+        .pipe(rename('_MinifiedLayout.html'))
+        .pipe(gulp.dest('SharedMinifications'));
 });
+
+gulp.task('default', gulp.series('minify-html'));
