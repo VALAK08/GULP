9391f8e126764c6ae1eff9d214301f8f2eb438b9
Ali Assim Mokdad <49646139+VALAK08@users.noreply.github.com>
2023-10-21 13:37:36 +0300

    Code  formatting

--- GULP/gulpfile.js
+++ GULP/gulpfile.js
@@ -8,71 +8,69 @@
     sass = require('gulp-sass')(require('sass')),
     uglify = require('gulp-uglify'),
 
-gulp.task('clean:css', function () {
-    return gulp.src('wwwroot/css/site.min.css', { read: false, allowEmpty: true })
-        .pipe(clean());
-});
+    //CSS Optimization
+    gulp.task('clean:css', function () {
+        return gulp.src('wwwroot/css/site.min.css', { read: false, allowEmpty: true })
+            .pipe(clean());
+    });
+    gulp.task('min-concat:css', function () {
+        return gulp.src('wwwroot/css/*.css')
+            .pipe(concat('site.min.css'))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest('wwwroot/css'));
+    });
 
-gulp.task('clean:js', function () {
-    return gulp.src('wwwroot/js/*.min.js', { read: false, allowEmpty: true })
-        .pipe(clean());
-});
+    //JS Optimization
+    gulp.task('clean:js', function () {
+        return gulp.src('wwwroot/js/*.min.js', { read: false, allowEmpty: true })
+            .pipe(clean());
+    });
+    gulp.task('min:js', function () {
+        return gulp.src('wwwroot/js/*.js')
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest('wwwroot/js/min'));
+    });
 
-//CSS Optimization
-gulp.task('min-concat:css', function () {
-    return gulp.src('wwwroot/css/*.css')
-        .pipe(concat('site.min.css'))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('wwwroot/css'));
-});
+    //Images Optimization   
 
-//JS Optimization
-gulp.task('min:js', function () {
-    return gulp.src('wwwroot/js/*.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('wwwroot/js/min'));
-});
+    //Did you forget to signal async completion?
+    //Gulp plugin that returns promises
+    //A Promise is an object representing the eventual completion or failure of an asynchronous operation
+    gulp.task('compress-images', async function () {
+        await gulp.src('wwwroot/images/*.{jpg,png}')
+            .pipe(imagemin())
+            .pipe(gulp.dest('wwwroot/images/compressedImages'))
+    });
 
-//Images Optimization
+    //HTML Optimization
+    gulp.task('minify-html', function () {
+        return gulp.src('Pages/Shared/_Layout.cshtml')
+            .pipe(minifyHtml())
+            .pipe(rename('_MinifiedLayout.html'))
+            .pipe(gulp.dest('SharedMinifications'));
+    });
 
-//Did you forget to signal async completion?
-//Gulp plugin that returns promises
-//A Promise is an object representing the eventual completion or failure of an asynchronous operation
-gulp.task('compress-images',async function () {
-    await gulp.src('wwwroot/images/*.{jpg,png}')
-        .pipe(imagemin())
-        .pipe(gulp.dest('wwwroot/images/compressedImages'))
-});
+    // SASS To CSS
+    function compileSass() {
+        return gulp.src('wwwroot/scss/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(rename('ConvertedCss.css'))
+            .pipe(gulp.dest('wwwroot/css'));
+    }
 
-//HTML Optimization
-gulp.task('minify-html', function () {
-    return gulp.src('Pages/Shared/_Layout.cshtml')
-        .pipe(minifyHtml())
-        .pipe(rename('_MinifiedLayout.html'))
-        .pipe(gulp.dest('SharedMinifications'));
-});
+    exports.compileSass = compileSass;
 
-// SASS To CSS
-function compileSass() {
-    return gulp.src('wwwroot/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rename('ConvertedCss.css'))
-        .pipe(gulp.dest('wwwroot/css'));
-}
+    gulp.task('default', gulp.series('clean:css', 'min-concat:css', 'clean:js',
+        'min:js', 'compress-images', 'minify-html', compileSass));
 
-exports.compileSass = compileSass;
-
-gulp.task('default', gulp.series('clean:css', 'min-concat:css', 'clean:js',
-    'min:js','compress-images', 'minify-html', compileSass));
-
-gulp.task('minifyCss', gulp.series('clean:css', 'min-concat:css'));
-gulp.task('minifyJs', gulp.series('clean:js', 'min:js'));
-gulp.task('minifyHtml', gulp.series('minify-html'));
-gulp.task('compressImages', gulp.series('compress-images'));
+    gulp.task('minifyCss', gulp.series('clean:css', 'min-concat:css'));
+    gulp.task('minifyJs', gulp.series('clean:js', 'min:js'));
+    gulp.task('minifyHtml', gulp.series('minify-html'));
+    gulp.task('compressImages', gulp.series('compress-images'));
 
 
 //Note(Video Optimization):
 // You can use Gulp in combination with video compression libraries to reduce the file size of videos.
-//Tools like FFmpeg or HandBrake can be integrated into your Gulp workflow to transcode videos 
+//Tools like FFmpeg or HandBrake can be integrated into your Gulp workflow to transcode videos
 //into more efficient formats, adjust bitrate, and reduce video quality to save space.
